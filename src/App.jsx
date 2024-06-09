6b8b73b30eed2be8a44c559b7f6beeed13e01be7
Hamza Riaz <hamzariazmohdkhan@gmail.com>
2024-06-09 21:58:31 +0500

    Changes to my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Hamza Riaz Khan",
+  title: "Graphic Designer & Cyber Security Analyst",
+  email: "hamzariazmohdkhan@gmail.com",
+  gitHub: "hamzariazkhan",
+  linkedIn: "hamzariazkhan",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "black";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,20 +24,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a student of Cyber Security at NED University Of Engineering And Technology and a Graphic Designer. I am enjoying new and creative design and to learn about security.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Web design", "C++", "Python", "Graphic Design", "C Language", "Linux"];
 
 /**
  * Use this to give more information about what you are passionate about,
@@ -45,7 +38,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about learning new things and dedicated to my cybersecurity studies. My interest in computer science drives me to explore new technologies and find innovative solutions to complex problems. I am always seeking to expand my knowledge and skillset, and I am excited to see what the future of cybersecurity holds.";
 
 const About = () => {
   return (
